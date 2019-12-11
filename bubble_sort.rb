2cdb675fb209d0fd03fa15ff4d17ea1bad128998
queeksm <queeksm@gmail.com>
2019-12-11 10:38:54 -0500

    ABC error

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 def bubble_sort(arr)
-  swap = true
-  while swap
-    swap = false
+  while true
+    out = true
     (arr.length - 1).times do |x|
-      swap = true if arr[x] > arr[x + 1]
+      out = false if arr[x] > arr[x + 1]
       arr[x], arr[x + 1] = arr[x + 1], arr[x] if arr[x] > arr[x + 1]
     end
+    break if out == true
   end
   print arr
 end
@@ -16,15 +16,15 @@ arr = [1, 4, 5, 66, 7, 3, 12_312, 2]
 
 bubble_sort(arr)
 
-def bubble_sort_by(arr)
-  swap = true
-  while swap
-    swap = false
+def bubble_sort_by(arr)  
+  while true
+    out = true
     (arr.length - 1).times do |x|
       b = arr[x + 1]
       arr[x], arr[x + 1] = arr[x + 1], arr[x] if yield(arr[x], b) >= 0
-      swap = true if yield(arr[x], b) >= 0
+      out = false if yield(arr[x], b) >= 0
     end
+    break if out == true
   end
   print arr
 end
