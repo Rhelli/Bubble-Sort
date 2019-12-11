943972a6354efbc802213d32f452bf198be00c66
queeksm <queeksm@gmail.com>
2019-12-11 10:42:28 -0500

    ABC errors part 2

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
+# rubocop:disable Metrics/AbcSize 
+
 def bubble_sort(arr)
-  while true
-    out = true
+  swap = true
+  while swap
+    swap = false
     (arr.length - 1).times do |x|
-      out = false if arr[x] > arr[x + 1]
+      swap = true if arr[x] > arr[x + 1]
       arr[x], arr[x + 1] = arr[x + 1], arr[x] if arr[x] > arr[x + 1]
     end
-    break if out == true
   end
   print arr
 end
@@ -16,17 +18,23 @@ arr = [1, 4, 5, 66, 7, 3, 12_312, 2]
 
 bubble_sort(arr)
 
-def bubble_sort_by(arr)  
-  while true
-    out = true
+def bubble_sort_by(arr)
+  swap = true
+  while swap
+    swap = false
     (arr.length - 1).times do |x|
       b = arr[x + 1]
       arr[x], arr[x + 1] = arr[x + 1], arr[x] if yield(arr[x], b) >= 0
-      out = false if yield(arr[x], b) >= 0
+      swap = true if yield(arr[x], b) >= 0
     end
-    break if out == true
   end
   print arr
 end
 
 bubble_sort_by(arr) { |a, b| a - b }
+
+bubble_sort_by(["hi","hello","hey"]) do |left,right| 
+  left.length - right.length 
+end 
+
+# rubocop:enable Metrics/AbcSize
