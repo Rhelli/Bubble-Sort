ef331a6af1f6bb171ac5ea5cd6d46acd16a1f5ea
queeksm <queeksm@gmail.com>
2019-12-11 10:17:39 -0500

    Additional linter errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,39 +1,34 @@
 # frozen_string_literal: true
+# rubocop:disable Metrics/ClassLength 
 
-def bubble_sort(arr)  
+def bubble_sort(arr)
   swap = true
-      while swap
-        swap = false
-        (arr.length - 1).times do |x|
-          swap = true if arr[x]>arr[x+1]
-          arr[x], arr[x + 1] = arr[x + 1], arr[x] if arr[x]>arr[x+1]
-        end
-      end
+  while swap
+    swap = false
+    (arr.length - 1).times do |x|
+      swap = true if arr[x] > arr[x+1]
+      arr[x], arr[x + 1] = arr[x + 1], arr[x] if arr[x] > arr[x + 1]
+    end
+  end
   print arr
 end
 
-arr = [1, 4, 5, 66, 7, 3, 12312, 2]
+arr = [1, 4, 5, 66, 7, 3, 12_312, 2]
 
 bubble_sort(arr)
 
-def bubble_sort_by(arr)  
+def bubble_sort_by(arr)
   swap = true
   while swap
     swap = false
     (arr.length - 1).times do |x|
       a = arr[x]
-      b = arr[x+1]
-      arr[x], arr[x+1] = arr[x+1],arr[x] if yield(a,b) >= 0
+      b = arr[x + 1]
+      arr[x], arr[x + 1] = arr[x + 1],arr[x] if yield(a,b) >= 0
       swap = true  if yield(a,b) >= 0
-    end    
+    end
   end
   print arr
 end
 
-bubble_sort_by(arr) {|a,b|   
-  a-b
-}
-
-
-
-
+bubble_sort_by(arr) {|a,b| a - b}
